2347c8a51fbac028d3f92a79b7bce25cde962811
Lisa Li <lisa11@users.noreply.github.com>
2017-01-18 19:35:32 -0600

    finished

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,15 +6,52 @@ class GameController < ApplicationController
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
     # ===============================================================
+    number = rand(0..2)
+    computer_move = "computer is thinking"
+    message = "no result yet"
+
+    #if 0, then rock
+    #if 1, then paper
+    #if 2, then scissor
+
+    if number == 0
+      computer_move = "rock"
+      if @user_move == "rock"
+        message = "tie"
+      elsif @user_move == "paper"
+        message = "win"
+      else
+        message = "lose"
+      end
+    elsif number == 1
+      computer_move = "paper"
+      if @user_move == "rock"
+        message = "lose"
+      elsif @user_move == "paper"
+        message = "tie"
+      else
+        message == "win"
+      end
+    elsif number == 2
+      computer_move = "scissors"
+      if @user_move == "rock"
+        message = "win"
+      elsif @user_move == "paper"
+        message = "lose"
+      else
+        message == "tie"
+      end
+    end
+
 
     # Your logic here
 
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = computer_move
 
-    @result = "Replace this string with the correct value."
+    @result = message
 
     # ===============================================================
     # Your code goes above.
